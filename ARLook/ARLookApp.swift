e8683512a797c6aefebab2271527919f47e1ab4c
Narek Aslanyan <aslanyan99nak>
2025-01-30 17:43:28 +0400

    Initial Commit

---
+++ ARLook/ARLookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARLookApp.swift
+//  ARLook
+//
+//  Created by Narek Aslanyan on 30.01.25.
+//
+
+import SwiftUI
+
+@main
+struct ARLookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
