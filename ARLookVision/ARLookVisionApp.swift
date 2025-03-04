aec826cbf482b40cf239aaa36fde5855dec25c7b
Narek Aslanyan <narekaslanyan@simplytechnologies.net>
2025-03-04 16:10:08 +0400

    Add Vision target to project

---
+++ ARLookVision/ARLookVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARLookVisionApp.swift
+//  ARLookVision
+//
+//  Created by Narek on 04.03.25.
+//
+
+import SwiftUI
+
+@main
+struct ARLookVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
